3453db4e534297835d45c730a3896824b8f02d8e
Katja Hyrynkangas <katja.hyrynkangas@hotmail.com>
2018-04-23 16:41:31 +0300

    Video 3_1

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
-// class App extends React.Component {
-//   render(){
-//     return <h1>Hello Series</h1>
-//   }
-// }
+class App extends React.Component {
+  render(){
+    return (
+      <div>
+        <h1>Hello Series</h1>
+        <b>Bold</b>
+      </div>
+    )
+  }
+}
 
-const App = () => <h1>Hello stateless</h1>
 
 export default App;
