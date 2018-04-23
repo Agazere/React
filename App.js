72e49b273e5ece4c3ff60560e29c1e51168fda96
Katja Hyrynkangas <katja.hyrynkangas@hotmail.com>
2018-04-23 17:01:16 +0300

    Video 4_2

--- App.js
+++ App.js
@@ -2,8 +2,19 @@ import React from 'react';
 
 class App extends React.Component {
   render(){
-    return <h1>{this.props.txt}</h1>
+    let txt = this.props.txt
+    return <h1>{txt}</h1>
   }
+}
+
+// App.propTypes = {
+//   txt: React.propTypes.string,
+//   cat: React.PropTypes.number.isRequired
+// }
+// Tämä ei toimi, koska React.PropTypes ei ole enää käytössä
+
+App.defaultProps = {
+  txt: "this is the default text"
 }
 
 
